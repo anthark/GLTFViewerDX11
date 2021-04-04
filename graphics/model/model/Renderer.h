0524aca67f4763dfc9657cfd514ee42c2fd3960a
MikhailNakhatovich <mish_1998@mail.ru>
2021-04-04 23:04:42 +0300

    Copied the previous laboratory

---
+++ graphics/model/model/Renderer.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include "DeviceResources.h"
+#include "RenderTexture.h"
+#include "ShaderStructures.h"
+#include "ToneMapPostProcess.h"
+#include "Camera.h"
+#include "Settings.h"
+
+class Renderer
+{
+public:
+    Renderer(const std::shared_ptr<DeviceResources>& deviceResources, const std::shared_ptr<Camera>& camera, const std::shared_ptr<Settings>& settings);
+    ~Renderer();
+
+    HRESULT CreateDeviceDependentResources();
+    HRESULT CreateWindowSizeDependentResources();
+    
+    HRESULT OnResize();
+
+    void Update();
+    void Render();
+
+private:
+    HRESULT CreateShaders();
+    HRESULT CreateSphere();
+    HRESULT CreateLights();
+    HRESULT CreateTexture();
+    HRESULT CreateCubeTexture();
+    HRESULT CreateIrradianceTexture();
+    HRESULT CreatePrefilteredColorTexture();
+    HRESULT CreatePreintegratedBRDFTexture();
+    HRESULT CreateCubeTextureFromResource(UINT size, ID3D11Texture2D* dst, ID3D11ShaderResourceView* src, ID3D11VertexShader* vs, ID3D11PixelShader* ps, UINT mipSlice = 0);
+
+    void UpdatePerspective();
+
+    void Clear();
+    void RenderInTexture();
+    void RenderEnvironment();
+    void PostProcessTexture();
+
+    std::unique_ptr<RenderTexture>        m_pRenderTexture;
+    std::unique_ptr<ToneMapPostProcess>   m_pToneMap;
+    std::shared_ptr<Camera>               m_pCamera;
+    std::shared_ptr<DeviceResources>      m_pDeviceResources;
+    std::shared_ptr<Settings>             m_pSettings;
+
+    Microsoft::WRL::ComPtr<ID3D11InputLayout>        m_pInputLayout;
+    Microsoft::WRL::ComPtr<ID3D11InputLayout>        m_pIBLInputLayout;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pVertexBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pEnvironmentVertexBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pIndexBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D>          m_pEnvironmentTexture;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_pEnvironmentShaderResourceView;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D>          m_pEnvironmentCubeTexture;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_pEnvironmentCubeShaderResourceView;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D>          m_pIrradianceTexture;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_pIrradianceShaderResourceView;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D>          m_pPrefilteredColorTexture;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_pPrefilteredColorShaderResourceView;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D>          m_pPreintegratedBRDFTexture;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_pPreintegratedBRDFShaderResourceView;
+    Microsoft::WRL::ComPtr<ID3D11VertexShader>       m_pPBRVertexShader;
+    Microsoft::WRL::ComPtr<ID3D11VertexShader>       m_pEnvironmentVertexShader;
+    Microsoft::WRL::ComPtr<ID3D11VertexShader>       m_pIBLVertexShader;
+    Microsoft::WRL::ComPtr<ID3D11VertexShader>       m_pEnvironmentCubeVertexShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pNDPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pGPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pFPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pEnvironmentPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pIrradiancePixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pPrefilteredColorPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pPreintegratedBRDFPixelShader;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader>        m_pEnvironmentCubePixelShader;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pConstantBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pLightPositionBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pLightColorBuffer;
+    Microsoft::WRL::ComPtr<ID3D11Buffer>             m_pMaterialBuffer;
+
+    std::vector<Microsoft::WRL::ComPtr<ID3D11SamplerState>> m_pSamplerStates;
+
+    WorldViewProjectionConstantBuffer m_constantBufferData;
+    LightPositionConstantBuffer       m_lightPositionBufferData;
+    LightColorConstantBuffer          m_lightColorBufferData;
+    MaterialConstantBuffer            m_materialBufferData;
+    
+    UINT32 m_indexCount;
+    UINT32 m_frameCount;
+
+    DirectX::XMVECTOR m_targers[6] = {
+        DirectX::XMVectorSet(1, 0, 0, 0),
+        DirectX::XMVectorSet(-1, 0, 0, 0),
+        DirectX::XMVectorSet(0, 1, 0, 0),
+        DirectX::XMVectorSet(0, -1, 0, 0),
+        DirectX::XMVectorSet(0, 0, 1, 0),
+        DirectX::XMVectorSet(0, 0, -1, 0)
+    };
+
+    DirectX::XMVECTOR m_ups[6] = {
+        DirectX::XMVectorSet(0, 1, 0, 0),
+        DirectX::XMVectorSet(0, 1, 0, 0),
+        DirectX::XMVectorSet(0, 0, -1, 0),
+        DirectX::XMVectorSet(0, 0, 1, 0),
+        DirectX::XMVectorSet(0, 1, 0, 0),
+        DirectX::XMVectorSet(0, 1, 0, 0)
+    };
+
+    DirectX::XMFLOAT3 m_squareLeftBottomAngles[6] = {
+        DirectX::XMFLOAT3(0.5f, -0.5f, 0.5f),
+        DirectX::XMFLOAT3(-0.5f, -0.5f, -0.5f),
+        DirectX::XMFLOAT3(-0.5f, 0.5f, 0.5f),
+        DirectX::XMFLOAT3(-0.5f, -0.5f, -0.5f),
+        DirectX::XMFLOAT3(-0.5f, -0.5f, 0.5f),
+        DirectX::XMFLOAT3(0.5f, -0.5f, -0.5f)
+    };
+
+    DirectX::XMFLOAT3 m_squareRightTopAngles[6] = {
+        DirectX::XMFLOAT3(0.5f, 0.5f, -0.5f),
+        DirectX::XMFLOAT3(-0.5f, 0.5f, 0.5f),
+        DirectX::XMFLOAT3(0.5f, 0.5f, -0.5f),
+        DirectX::XMFLOAT3(0.5f, -0.5f, 0.5f),
+        DirectX::XMFLOAT3(0.5f, 0.5f, 0.5f),
+        DirectX::XMFLOAT3(-0.5f, 0.5f, -0.5f)
+    };
+};
---
+++ graphics/model/model/Settings.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+
+#include "Settings.h"
+#include "../../ImGui/imgui_impl_dx11.h"
+#include "../../ImGui/imgui_impl_win32.h"
+
+Settings::Settings(const std::shared_ptr<DeviceResources>& deviceResources) :
+    m_pDeviceResources(deviceResources),
+    m_shaderMode(PBRShaderMode::REGULAR),
+    m_metalType(MetalType::ALUMINIUM),
+    m_strengths()
+{};
+
+void Settings::CreateResources(HWND hWnd)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui_ImplWin32_Init(hWnd);
+    ImGui_ImplDX11_Init(m_pDeviceResources->GetDevice(), m_pDeviceResources->GetDeviceContext());
+    ImGui::StyleColorsDark();
+}
+
+void Settings::Render()
+{
+    ImGui_ImplDX11_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Once);
+    ImGui::SetNextWindowSize(ImVec2(500, 175), ImGuiCond_Once);
+    ImGui::Begin("Settings");
+
+    static const char* shaderModes[] = { "Regular", "Normal distribution", "Geometry", "Fresnel" };
+    ImGui::Combo("Shader mode", reinterpret_cast<int*>(&m_shaderMode), shaderModes, IM_ARRAYSIZE(shaderModes));
+
+    static const char* metalTypes[] = { "Aluminium", "Argentum", "Aurum", "Cuprum", "Ferrum" };
+    ImGui::Combo("Metal type", reinterpret_cast<int*>(&m_metalType), metalTypes, IM_ARRAYSIZE(metalTypes));
+
+    ImGui::ColorEdit3("Albedo", m_albedo);
+
+    for (size_t i = 0; i < NUM_LIGHTS; ++i)
+        ImGui::SliderFloat((std::string("Strength of ") + std::to_string(i) + std::string(" light")).c_str(), m_strengths + i, 0.0f, 1000.0f);
+
+    ImGui::End();
+
+    ImGui::Render();
+    
+    ID3D11RenderTargetView* renderTarget = m_pDeviceResources->GetRenderTarget();
+    ID3D11DepthStencilView* depthStencil = m_pDeviceResources->GetDepthStencil();
+    m_pDeviceResources->GetDeviceContext()->OMSetRenderTargets(1, &renderTarget, depthStencil);
+    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+}
+
+float Settings::GetLightStrength(UINT index) const
+{
+    if (index >= NUM_LIGHTS)
+        return 0.0f;
+    return m_strengths[index];
+}
+
+Settings::~Settings()
+{
+    ImGui_ImplDX11_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
+}
