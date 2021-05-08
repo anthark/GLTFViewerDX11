603335a22246a4c0d60a825872a3e4692a3a1d66
Kurnosov Dmitriy <dima2202888@yandex.ru>
2021-05-08 15:16:38 +0300

    Copied previous lab and added scene

---
+++ graphics/shadows/shadows/Settings.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "DeviceResources.h"
+#include "ShaderStructures.h"
+#include "../../ImGui/imgui.h"
+
+class Settings
+{
+public:
+    enum class SETTINGS_PBR_SHADER_MODE
+    {
+        REGULAR = 0,
+        NORMAL_DISTRIBUTION,
+        GEOMETRY,
+        FRESNEL
+    };
+
+    enum class SETTINGS_SCENE_MODE
+    {
+        MODEL = 0,
+        SPHERE
+    };
+
+    Settings(const std::shared_ptr<DeviceResources>& deviceResources);
+    ~Settings();
+
+    void CreateResources(HWND hWnd);
+
+    SETTINGS_PBR_SHADER_MODE GetShaderMode() const { return m_shaderMode; };
+    SETTINGS_SCENE_MODE GetSceneMode() const { return m_sceneMode; };
+
+    DirectX::XMFLOAT4 GetLightColor(UINT index) const;
+    DirectX::XMFLOAT4 GetLightPosition(UINT index) const;
+    DirectX::XMFLOAT4 GetLightAttenuation(UINT index) const;
+
+    DirectX::XMFLOAT4 GetAlbedo() const { return DirectX::XMFLOAT4(m_albedo); };
+    FLOAT GetMetalness() const { return m_metalRough[0]; };
+    FLOAT GetRoughness() const { return m_metalRough[1]; };
+
+    void Render();
+
+private:
+    std::shared_ptr<DeviceResources> m_pDeviceResources;
+
+    SETTINGS_PBR_SHADER_MODE m_shaderMode;
+    SETTINGS_SCENE_MODE      m_sceneMode;
+
+    float m_lightsStrengths[NUM_LIGHTS];
+    float m_lightsThetaAngles[NUM_LIGHTS];
+    float m_lightsPhiAngles[NUM_LIGHTS];
+    float m_lightsDistances[NUM_LIGHTS];
+    float m_lightsColors[NUM_LIGHTS][3];
+    float m_lightsAttenuations[NUM_LIGHTS][3];
+    
+    float m_albedo[4] = { 0.25f, 1.0f, 1.0f, 1.0f };
+    float m_metalRough[2];
+};
